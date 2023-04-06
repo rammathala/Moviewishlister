25528dbd67f02acf4485e0e86097f1dde35b8944
Ram Mathala <98813593+rammathala@users.noreply.github.com>
2023-04-06 11:22:59 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBCifUrNb4_4hmxrzXR_obYzo7Dk1EX08k",
-  authDomain: "movieapp-c59f1.firebaseapp.com",
-  projectId: "movieapp-c59f1",
-  storageBucket: "movieapp-c59f1.appspot.com",
-  messagingSenderId: "327437158620",
-  appId: "1:327437158620:web:166c54b28cc78b5a6b4c19"
+/* All your firebase configuration lines*/
 };
 
 // Use this to initialize the firebase App
